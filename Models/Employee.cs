e1dd4f8ca4744abc6f0043afa0bcb9d466cbdf9e
Corey Schroeder <codingcorey365@gmail.com>
2024-09-20 17:52:07 -0500

    added the data objects to the employee model class.

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -2,5 +2,30 @@
 
 public class Employee
 {
-    
+    //EmployeeID
+    public int EmployeeId { get; set; }
+
+    //Name
+    public string? FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string? LastName { get; set; }
+
+    //Birthday
+    public int BirthDay { get; set; }
+    public int BirthMonth { get; set; }
+    public int BirthYear { get; set; }
+
+    //Age
+    public int Age { get; set; }
+
+    //Contact
+    public string? PhoneNumber { get; set; }
+    public string? EmailAddress { get; set; }
+    public string? HomeAddress { get; set; }
+
+    //Current Position
+    public string? EmployeeDepartment { get; set; }
+    public int EmployeeTitle { get; set; }
+    public int PayRate { get; set; }
+    public int HoursWorked { get; set; }
 }
