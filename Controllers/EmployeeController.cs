da6abb678de1d59c60d2c753344d07a297375f36
Corey Schroeder <codingcorey365@gmail.com>
2024-09-20 17:45:51 -0500

    added the EmployeeController and the Employeeview folder with all current subsequent/associated view files

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
