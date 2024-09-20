5b7d12e796cd4f1e779c14c46c4c1c24d99539d0
Corey Schroeder <codingcorey365@gmail.com>
2024-09-20 17:53:57 -0500

    added the stubbed out methods to the EmployeeInterface.

--- Interface/IEmployeeInterface.cs
+++ Interface/IEmployeeInterface.cs
@@ -1,6 +1,12 @@
-﻿namespace EmployeeProject.Interface;
+﻿using EmployeeProject.Models;
+
+namespace EmployeeProject.Interface;
 
 public interface IEmployeeInterface
 {
-    
+    public void CreateEmployee(Employee employeeToInsert);
+    public Employee GetEmployeeById(int id);
+    public IEnumerable<Employee> GetAllEmployees();
+    public void UpdateEmployee(Employee employee);
+    public void DeleteEmployee(Employee employee);
 }
